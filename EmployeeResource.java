10f20b6ab0336b70c94c9ad87c27870674a83142
sang-ee <69801991+sang-ee@users.noreply.github.com>
2020-08-27 19:40:20 +0530

    Update EmployeeResource.java

--- EmployeeResource.java
+++ EmployeeResource.java
@@ -1,6 +1,6 @@
-package com.tutorialspoint;
+package com.Jerseyy;
 
-import java.util.Arrays;
+
 import java.util.List;
 
 import javax.ws.rs.DELETE;
@@ -19,7 +19,6 @@ public class EmployeeResource {
 	@Produces(MediaType.APPLICATION_XML)
 	
 public List<Employee> getEmpl(){
-	
 	return repo.getEmps();
 	
 }
@@ -38,29 +37,42 @@ public Employee getEmployee1(@PathParam("id") int id){
 @Path("emp")
 @Produces(MediaType.APPLICATION_XML)
 public Employee createEmployee(Employee a1){
-	repo.create(a1);
+	boolean val=false;
+	val=repo.create(a1);
+	if(val)
 	return a1;
-	
+	else 
+		return null;
 	
 }
 @PUT
 @Path("emp")
 @Produces(MediaType.APPLICATION_XML)
 public Employee updateEmployee(Employee a1){
-	repo.update(a1);
+	boolean val1=false;
+	val1=repo.update(a1);
+	if(val1)
 	return a1;
-	
+	else 
+		return null;
 	
 }
 @DELETE
 @Path("emp/{id}")
+@Produces(MediaType.APPLICATION_XML)
 public Employee delEmployee(@PathParam("id") int id){
+	boolean val=false;
 	Employee a=repo.getEmployee(id);
 	if(a.getId()!=0){
-		repo.delete(id);
-	}
-	return a;
+		
+		val=repo.delete(id);
+		if(val)
+		return a;
+	
+	else 
+		return null;
 }
-
+	else return null;
+}
 
 }
