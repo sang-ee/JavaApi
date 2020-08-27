75eef5b7bd69635eece0fd29c6c6f678c75b1cc5
sang-ee <69801991+sang-ee@users.noreply.github.com>
2020-08-27 19:39:04 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,4 +1,4 @@
-package com.tutorialspoint;
+package com.Jerseyy;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -6,7 +6,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Employee {
 	private int id;
 private String name;
-private int points;
+private String dept;
+private String mobile_no;
+private String mail_id;
+private String address;
 public String getName() {
 	return name;
 }
@@ -19,11 +22,30 @@ public int getId() {
 public void setId(int id) {
 	this.id = id;
 }
-public int getPoints() {
-	return points;
+public String getDept() {
+	return dept;
 }
-public void setPoints(int points) {
-	this.points = points;
+public void setDept(String dept) {
+	this.dept = dept;
 }
+public String getMobileNo() {
+	return mobile_no;
+}
+public void setMobileNo(String mobile_no) {
+	this.mobile_no = mobile_no;
+}
+public String getMailId() {
+	return mail_id;
+}
+public void setMailId(String mail_id) {
+	this.mail_id = mail_id;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+
 
 }
